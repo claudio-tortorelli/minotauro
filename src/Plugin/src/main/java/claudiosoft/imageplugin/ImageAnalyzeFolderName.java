36ac22ec90bc315a5d545050eb42aacd8a49f36f
claudio-tortorelli <torto78@gmail.com>
2025-04-02 22:00:06 +0200

    added more plugins

---
+++ src/Plugin/src/main/java/claudiosoft/imageplugin/ImageAnalyzeFolderName.java
@@ -0,0 +1,38 @@
+package claudiosoft.imageplugin;
+
+import claudiosoft.commons.CTException;
+import claudiosoft.commons.Config;
+import claudiosoft.transientimage.TransientImageProvider;
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author claudio.tortorelli
+ */
+public class ImageAnalyzeFolderName extends BaseImagePlugin {
+
+    public ImageAnalyzeFolderName(int step) {
+        super(step);
+    }
+
+    @Override
+    public void init(Config config, TransientImageProvider transientImageProvider) throws CTException {
+        super.init(config, transientImageProvider);
+
+    }
+
+    @Override
+    public void apply(File image) throws CTException {
+        super.apply(image);
+
+    }
+
+    protected void store(String result) throws NoSuchAlgorithmException, IOException, CTException {
+//        TransientImage transientImage = transientImageProvider.get(imageFile);
+//        transientImage.set(this.getClass().getSimpleName(), "id", result);
+//        transientImage.store();
+    }
+
+}
